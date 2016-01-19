1b7ba38d934989df6a222523d25da376fa864e4e
Aaron Cimolini <aaron@cimolini.com>
2016-01-19 12:37:28 -0500

    Initial commit and basic work done for scaffolding the main page.

---
+++ test/spec/test.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    // var assert = require('assert');
+
+    describe('Give it some context', function () {
+        describe('maybe a bit more context here', function () {
+            it('should run here few assertions', function () {
+
+                assert.equal(testing, 'string');
+
+            });
+        });
+    });
+})();
