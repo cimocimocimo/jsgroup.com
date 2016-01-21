27b3b96f64309e6d6f3bf1f10a63f7c60aa9b887
Aaron Cimolini <aaron@cimolini.com>
2016-01-21 17:10:32 -0500

    Adds the slick carousel to the home page.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,36 @@
-console.log('\'Allo \'Allo!');
+var jsGroup = (function($, smoothScroll, window, document, undefined){
+    function publicInit(){
+        console.log('publicInit');
+
+        smoothScroll.init({
+            offset: 16,
+            speed: 700
+        });
+
+        // site header
+        // cache.siteHeader.headroom({
+        //     "offset": options.plugins.headroom.offset,
+        //     "tolerance": options.plugins.headroom.tolerance
+        // });
+
+        // carousel
+        $('#carousel-main').slick({
+            prevArrow: '<a href="#" class="slick-prev"></a>',
+            nextArrow: '<a href="#" class="slick-next"></a>',
+            autoplay: true,
+            fade: true,
+            autoplaySpeed: 3500,
+            speed: 1500
+        });
+
+    }
+
+    return {
+        init: publicInit
+    };
+
+})(jQuery, smoothScroll, window, document);
+
+jQuery(function(){
+    jsGroup.init({});
+});
